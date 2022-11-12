8cf6f7fd92e51c2ada43419505a17da961ed3b4d
k4ds3 <5180118+k4ds3@users.noreply.github.com>
2022-11-12 21:41:28 +0100

    feat: add 'look_for_alphanumeric_id' flag to enable parsing values that are not only digits

--- autolink_references/main.py
+++ autolink_references/main.py
@@ -3,11 +3,15 @@ from mkdocs.plugins import BasePlugin
 from mkdocs.config import config_options
 
 
-def replace_autolink_references(markdown, reference_prefix, target_url):
+def replace_autolink_references(markdown, reference_prefix, target_url, look_for_alphanumeric_id=False):
     if "<num>" not in reference_prefix:
         reference_prefix = reference_prefix + "<num>"
 
-    find_regex = reference_prefix.replace("<num>", "(?P<num>[0-9]+)")
+    if look_for_alphanumeric_id:
+        find_regex = reference_prefix.replace("<num>", "(?P<num>[A-Za-z0-9-_/]+)")
+    else:
+        find_regex = reference_prefix.replace("<num>", "(?P<num>[0-9]+)")
+
     find_regex = (
         "(?P<b>\\[)?(?P<text>" + find_regex + ")(?(b)\\])(?(b)(?:\\((?P<url>.*?)\\))?)"
     )
@@ -42,6 +46,8 @@ class AutoLinkOption(config_options.OptionallyRequired):
                 )
             if "<num>" not in autolink["target_url"]:
                 raise config_options.ValidationError("Missing '<num>' in 'target_url'.")
+            if "look_for_alphanumeric_id" not in autolink:
+                autolink["look_for_alphanumeric_id"] = False
 
         return values
 
@@ -62,7 +68,7 @@ class AutolinkReference(BasePlugin):
         """
         for autolink in self.config["autolinks"]:
             markdown = replace_autolink_references(
-                markdown, autolink["reference_prefix"], autolink["target_url"]
+                markdown, autolink["reference_prefix"], autolink["target_url"], autolink["look_for_alphanumeric_id"]
             )
 
         return markdown
